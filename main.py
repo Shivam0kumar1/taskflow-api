71eb6a75e8298f45523111c6babb42b82adaba1b
Shivam <shivam.sk754@gamil.com>
2026-06-22 21:11:49 +0530

    "commenting init_postgresql()"

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from logger import logger
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    from init_postgres import init_postgresql
-    # init_db() # PostgreSQL tables already created
-    init_postgresql()
+    # from init_postgres import init_postgresql
+    # init_postgresql()
     yield
 
 app = FastAPI(lifespan=lifespan)
